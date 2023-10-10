ac4714bd7a9a9b4d51b9bec2f7647c9263229981
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-10-10 08:37:32 +0000

    Update keycloak-admin-client to 22.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
-  lazy val keycloakAdminClient = "org.keycloak" % "keycloak-admin-client" % "22.0.3"
+  lazy val keycloakAdminClient = "org.keycloak" % "keycloak-admin-client" % "22.0.4"
   lazy val lambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.27"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
