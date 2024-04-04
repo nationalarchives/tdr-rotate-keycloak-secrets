7ffe0c2e27b794bb1ae69f23ed0f8529d01666c4
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-04-04 08:27:07 +0000

    Update mockito-scala to 1.17.31 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
   lazy val keycloakAdminClient = "org.keycloak" % "keycloak-admin-client" % "24.0.2"
   lazy val lambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
-  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.30"
+  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.31"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
   lazy val slf4j = "org.slf4j" % "slf4j-simple" % "2.0.12"
   lazy val typesafe = "com.typesafe" % "config" % "1.4.3"
