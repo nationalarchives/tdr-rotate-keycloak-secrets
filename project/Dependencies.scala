805afabca286f84f90a3cc330125ae037733a533
thanhz <thanhz@users.noreply.github.com>
2024-04-22 18:15:54 +0100

    Scala steward dependencies (#173)
    * Update scala-library to 2.13.13 in scala-steward-dependencies
    
    * Update sbt-assembly to 2.2.0 in scala-steward-dependencies
    
    * Update mockito-scala to 1.17.31 in scala-steward-dependencies
    
    * Update slf4j-simple to 2.0.13 in scala-steward-dependencies
    
    * Update keycloak-admin-client to 24.0.3 in scala-steward-dependencies
    
    * Update ecs, sns, ssm to 2.25.35 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,17 +3,17 @@ import sbt._
 object Dependencies {
   private val circeVersion = "0.14.6"
 
-  lazy val awsSns = "software.amazon.awssdk" % "sns" % "2.24.13"
-  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.24.13"
-  lazy val awsEcs = "software.amazon.awssdk" % "ecs" % "2.24.13"
+  lazy val awsSns = "software.amazon.awssdk" % "sns" % "2.25.35"
+  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.25.35"
+  lazy val awsEcs = "software.amazon.awssdk" % "ecs" % "2.25.35"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
-  lazy val keycloakAdminClient = "org.keycloak" % "keycloak-admin-client" % "23.0.7"
+  lazy val keycloakAdminClient = "org.keycloak" % "keycloak-admin-client" % "24.0.3"
   lazy val lambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
-  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.30"
+  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.31"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
-  lazy val slf4j = "org.slf4j" % "slf4j-simple" % "2.0.12"
+  lazy val slf4j = "org.slf4j" % "slf4j-simple" % "2.0.13"
   lazy val typesafe = "com.typesafe" % "config" % "1.4.3"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.1"
 }
