b60f341040af6c6c7d2d6f499347fcab489b30f3
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-05-16 08:26:53 +0000

    Update ecs, sns, ssm to 2.25.53 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependencies {
   private val circeVersion = "0.14.7"
 
-  lazy val awsSns = "software.amazon.awssdk" % "sns" % "2.25.50"
-  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.25.50"
-  lazy val awsEcs = "software.amazon.awssdk" % "ecs" % "2.25.50"
+  lazy val awsSns = "software.amazon.awssdk" % "sns" % "2.25.53"
+  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.25.53"
+  lazy val awsEcs = "software.amazon.awssdk" % "ecs" % "2.25.53"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
